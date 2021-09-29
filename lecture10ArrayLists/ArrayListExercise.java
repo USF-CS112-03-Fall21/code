d1e338069abdd2387e39f0f9029649a5d7eecf81
Olga Karpenko <okarpenko@usfca.edu>
2021-09-29 12:43:30 -0700

    Added exercises

---
+++ lecture10ArrayLists/ArrayListExercise.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class ArrayListExercise {
+	
+	public int findMin(ArrayList<Integer> list) {
+		// FILL IN CODE
+
+		return 0; // change
+	}
+
+
+	public void changeRadii(ArrayList<Circle> circles, double value) {
+		// Add value to the radius of each circle
+		// FILL IN CODE
+	}
+
+	public static void main(String[] args) {
+		// Create an ArrayList with several integer values
+		// call findMin and print the result
+
+		// Create an ArrayList of Circles
+		// call changeRadii
+	}
+}
---
+++ lecture10ArrayLists/Circle.java
@@ -0,0 +1,15 @@
+public class Circle {
+	private double radius;
+
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+	public void setRadius(double newRad) {
+		radius = newRad;
+	}
+
+	public String toString() {
+		return "" + radius;
+	}
+}
