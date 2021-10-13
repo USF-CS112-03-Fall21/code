534d90019064fbe2326501eb1b39c3a537521aec
Olga Karpenko <okarpenko@usfca.edu>
2021-10-13 10:25:11 -0700

    Updated ThrowsClauseDemo

--- lecture13Exceptions/ThrowsClauseDemo.java
+++ lecture13Exceptions/ThrowsClauseDemo.java
@@ -6,8 +6,24 @@ import java.io.IOException;
 
 public class ThrowsClauseDemo {
 
+	/** Reads lines from the input file and prints them to the console. Handles IOException. */
+	public static void readFromFileHandlingException(String input) {
+		try(FileReader fileReader = new FileReader(input)) {  
+			BufferedReader br = new BufferedReader(fileReader);
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}	
+				
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+
 	/** Reads lines from the input file and prints them to the console. Does not catch IOException. */
-	public static void readFromFileNotHandlingException(String input) throws IOException{
+	public static void readFromFileNotHandlingException(String input) throws IOException {
 		FileReader fileReader = new FileReader(input);  
 		BufferedReader br = new BufferedReader(fileReader);
 		String line;
@@ -22,8 +38,12 @@ public class ThrowsClauseDemo {
 	
 
 	public static void main(String[] args) {
+
+		readFromFileHandlingException("file.txt"); // IOException handled inside the method
+
+		// IOException handled in main
 		try {
-			readFromFileNotHandlingException("file.txt"); 
+			readFromFileNotHandlingException("file.txt");  
 		}
 		catch (IOException e) {
 			System.out.println(e);
